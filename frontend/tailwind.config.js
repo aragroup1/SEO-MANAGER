eb893e8aa35ffd307dea838e0b8fc44acdb6f7ba
aragroup1 <89531812+aragroup1@users.noreply.github.com>
2025-11-25 10:23:50 +0000

    Add Tailwind CSS configuration file

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+// frontend/tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      animation: {
+        blob: "blob 7s infinite",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
