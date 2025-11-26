b4b8b7bf2b9cdb686c3a518fc422105f799efc7a
aragroup1 <89531812+aragroup1@users.noreply.github.com>
2025-11-26 08:42:21 +0000

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,18 @@
+// frontend/next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://seo-manager-production.up.railway.app'
   },
-
+  // If you need server runtime config, use this syntax:
+  serverRuntimeConfig: {
+    mySecret: 'secret',
+    port: process.env.PORT || 8080
+  },
+  publicRuntimeConfig: {
+    apiUrl: process.env.NEXT_PUBLIC_API_URL || 'https://seo-manager-production.up.railway.app'
+  }
 }
 
 module.exports = nextConfig
